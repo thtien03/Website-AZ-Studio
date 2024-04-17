59af2f5c616ae21a1ac889530a62c7bb630a101e
thtien03 <118674266+thtien03@users.noreply.github.com>
2024-04-17 15:00:12 +0700

    upcode

---
+++ AZ_Studio/DoAnMonHoc/Controllers/CustomerVisitController.cs
@@ -0,0 +1,43 @@
+﻿using DoAnMonHoc.Data;
+using DoAnMonHoc.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoAnMonHoc.Controllers
+{
+	public class CustomerVisitController : Controller
+	{
+        private readonly ApplicationDbContext _context;
+
+        public CustomerVisitController(ApplicationDbContext context) {
+            this._context = context;
+        }
+		[Authorize(Roles = "admin")]
+		public async Task<IActionResult> Index()
+		{
+			var listMessage = await _context.messageCustomerVisit.ToListAsync();
+			return View(listMessage);
+		}
+		public async Task<IActionResult> ThongTinLienHe(MessageCustomerVisit model) 
+		{
+            try
+			{
+				var message = new MessageCustomerVisit
+				{
+					HoTen = model.HoTen,
+					Email = model.Email,
+					SoDienThoai = model.SoDienThoai,
+					TinNhan = model.TinNhan
+				};
+				await _context.messageCustomerVisit.AddAsync(message);
+				await _context.SaveChangesAsync();
+			}
+			catch(Exception ex)
+			{
+				return View(ex.Message);
+			}
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
---
+++ AZ_Studio/DoAnMonHoc/ViewModel/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoAnMonHoc.ViewModel
+{
+	public class AppUser : IdentityUser
+	{
+		[StringLength(100)]
+		[MaxLength(100)]
+		[Required]
+		public string? Name { get; set; }
+		public string? Address { get; set; }
+		public string? Role { get; set; }
+	}
+}
